734d2afa342218d729d5256c502489a4f4c6e129
Shubham Yadav <shubham005.sam@gmail.com>
2023-10-14 14:19:43 +0530

    Swagger file added

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module TakeOff_Task-2
 
 go 1.21.1
 
-require github.com/gorilla/mux v1.8.0 
+require github.com/gorilla/mux v1.8.0
--- models/employee.go
+++ models/employee.go
@@ -5,7 +5,7 @@ type Employee struct {
 	FirstName string `json:"first_name" csv:"first_name"`
 	LastName  string `json:"last_name" csv:"last_name"`
 	Email     string `json:"email" csv:"email"`
-	Password  string `json:"-" csv:"password"`
+	Password  string `json:"password" csv:"password"`
 	PhoneNo   string `json:"phoneNo" csv:"phoneNo"`
 	Role      string `json:"role" csv:"role"`
 }
